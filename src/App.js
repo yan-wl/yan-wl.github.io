c863ccac1d4c17114e57f57d3818a019f5992978
uberSaiyan <yanwl.note@gmail.com>
2020-02-04 20:15:57 +0800

    Add weather

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Blog from './components/blog/Blog';
-import Dashboard from './components/dashboard/Dashboard';
+import Weather from './components/Weather'
+import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
-    <Blog />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://google.com/"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div>
+      <CssBaseline />
+      <Weather />
+    </div>
   );
 }
 
