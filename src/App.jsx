2480168233df5df195b70cebae7b1dfd5040c192
uberSaiyan <yanwl.note@gmail.com>
2020-02-22 22:33:48 +0800

    Revamp look

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import './App.css';
+import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <Content />
-      <Footer />
-    </>
+    <div className="layout">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="content">
+        <Content />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../css/Header.css';
+
+export default function Header() {
+  return (
+    <div className="header-box" />
+  );
+}
